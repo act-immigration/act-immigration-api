b5287b31ba721ee465fb2dc71a519c4a58e17d2f
RileyManda <rileymanda69@gmail.com>
2024-03-19 11:56:44 +0200

    Implemented account object

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,3 @@
 // Empty manifest file for API-only applicationp
+//= link graphiql/rails/application.css
+//= link graphiql/rails/application.js
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module ActApi
     config.autoload_lib(ignore: %w(assets tasks))
     # Configuration for the application, engines, and railties goes here.
     #
+    require "sprockets/railtie"
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
