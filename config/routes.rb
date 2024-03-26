d2c4b8f2eb5eaf984234743fc6abb480198a8d28
RileyManda <rileymanda69@gmail.com>
2024-03-26 21:37:57 +0200

    Bug fixes and Updated session controller to handle auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
   post "/graphql", to: "graphql#execute"
   post "/enquiries", to: "enquiries#create"
