1d55fb55d2bcde6a84e86e24d4d64ed67c6c4588
RileyManda <rileymanda69@gmail.com>
2024-04-10 20:53:53 +0200

    Updated user_id key o accept null values if users is not signup

--- db/migrate/20240404092042_add_user_id_to_appointments.rb
+++ db/migrate/20240404092042_add_user_id_to_appointments.rb
@@ -1,5 +1,5 @@
 class AddUserIdToAppointments < ActiveRecord::Migration[7.1]
   def change
-    add_reference :appointments, :user, null: false, foreign_key: true
+    add_reference :appointments, :user, null: true, foreign_key: true
   end
 end
